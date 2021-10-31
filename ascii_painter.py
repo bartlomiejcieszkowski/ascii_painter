f2d70fd825cf6f9d14f152db1e12397bdebe1916
Bartłomiej Cieszkowski <bartlomiej.cieszkowski@gmail.com>
2021-10-31 13:37:28 +0100

    ascii_painter_engine as submodule

--- ascii_painter.py
+++ ascii_painter.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python3
 
-import sys
-
-try:
-    import ascii_painter_engine
-except Exception as e:
-    sys.path.insert(1, '..\\')
-    print(sys.path)
-    import ascii_painter_engine
+from ascii_painter_engine.main import ConsoleWidgetAlignment, ConsoleView, ConsoleWidgets
 
 print('success!')
+
+def main():
+    console_view = ConsoleView(debug=True)
+    console_view.color_mode()
+
+    # TODO: Percent of window, fill
+    pane = ConsoleWidgets.Pane(console_view=console_view, x=0, y=0, height=40, width=100,
+                                 alignment=ConsoleWidgetAlignment.LEFT_TOP)
+    pane.title = 'ASCII Painter'
+
+    console_view.add_widget(pane)
+
+    console_view.loop(True)
+
+if __name__ == '__main__':
+    main()
