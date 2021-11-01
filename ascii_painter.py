385f0ddb6caecf5620ef8676a24ebb2a14a41963
Bartłomiej Cieszkowski <bartlomiej.cieszkowski@gmail.com>
2021-11-01 07:05:34 +0100

    Pane taking whole window

--- ascii_painter.py
+++ ascii_painter.py
@@ -9,8 +9,8 @@ def main():
     console_view.color_mode()
 
     # TODO: Percent of window, fill
-    pane = ConsoleWidgets.Pane(console_view=console_view, x=0, y=0, height=40, width=100,
-                                 alignment=ConsoleWidgetAlignment.LEFT_TOP)
+    pane = ConsoleWidgets.Pane(console_view=console_view, x=1, y=1, height=100, width=100,
+                                 alignment=ConsoleWidgetAlignment.LEFT_TOP, percent=True)
     pane.title = 'ASCII Painter'
 
     console_view.add_widget(pane)
