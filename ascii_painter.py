1d78daacecb3d972adcb1e98691437bd8d7f6e24
Bartłomiej Cieszkowski <bartlomiej.cieszkowski@gmail.com>
2021-11-01 21:51:21 +0100

    change import, x=0, formatting

--- ascii_painter.py
+++ ascii_painter.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python3
 
-from ascii_painter_engine.main import ConsoleWidgetAlignment, ConsoleView, ConsoleWidgets
+import ascii_painter_engine as ape
 
 print('success!')
 
+
 def main():
-    console_view = ConsoleView(debug=True)
+    console_view = ape.ConsoleView(debug=True)
     console_view.color_mode()
 
     # TODO: Percent of window, fill
-    pane = ConsoleWidgets.Pane(console_view=console_view, x=1, y=1, height=100, width=100,
-                                 alignment=ConsoleWidgetAlignment.LEFT_TOP, percent=True)
+    pane = ape.ConsoleWidgets.Pane(console_view=console_view, x=0, y=1, height=100, width=100,
+                                   alignment=ape.ConsoleWidgetAlignment.LEFT_TOP, percent=True)
     pane.title = 'ASCII Painter'
 
     console_view.add_widget(pane)
 
     console_view.loop(True)
+
 
 if __name__ == '__main__':
     main()
