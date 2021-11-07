84c052208e37c0cc466271b05cff7d28d7dbc373
Bartłomiej Cieszkowski <bartlomiej.cieszkowski@gmail.com>
2021-11-07 22:27:29 +0100

    brush dummy widget

--- ascii_painter.py
+++ ascii_painter.py
@@ -7,7 +7,8 @@ print('success!')
 
 class Colors8BitPalette(ape.ConsoleWidgets.BorderWidget):
     def __init__(self, console_view, x: int, y: int,
-                 alignment: ape.Alignment, dimensions: ape.DimensionsFlag = ape.DimensionsFlag.Absolute, borderless: bool = False):
+                 alignment: ape.Alignment, dimensions: ape.DimensionsFlag = ape.DimensionsFlag.Absolute,
+                 borderless: bool = False):
         border = (0 if borderless else 2)
         width = 8 * 2 + border
         height = 2 + border
@@ -28,8 +29,36 @@ class Colors8BitPalette(ape.ConsoleWidgets.BorderWidget):
         for h in range(start, end):
             self.console_view.brush.MoveCursor(row=offset_rows + h)
             line = offset_str
-            for i in range(h*8, (h+1)*8):
+            for i in range(h * 8, (h + 1) * 8):
                 line += self.console_view.brush.BgColor(color=i, bits=ape.Brush.ColorBits.Bit8) + '  '
+            self.console_view.brush.print(line + self.console_view.brush.ResetColor(), end='')
+
+
+class BrushWidget(ape.ConsoleWidgets.BorderWidget):
+    def __init__(self, console_view, x: int, y: int,
+                 alignment: ape.Alignment, dimensions: ape.DimensionsFlag = ape.DimensionsFlag.Absolute,
+                 borderless: bool = False):
+        border = (0 if borderless else 2)
+        width = 3 * 2 + border
+        height = 2 + border
+        super().__init__(console_view=console_view, x=x, y=y, width=width, height=height, alignment=alignment,
+                         dimensions=dimensions, borderless=borderless)
+        self.title = 'Brush'
+
+    def draw(self):
+        super().draw()
+        border = 0 if self.borderless else 1
+        offset_rows = self.last_dimensions.row + border
+        offset_cols = self.last_dimensions.column + border
+        width = self.last_dimensions.width - (border * 2)
+        height = self.last_dimensions.height - (border * 2)
+        start = 0
+        end = height
+        offset_str = self.console_view.brush.MoveRight(offset_cols)
+        for h in range(start, end):
+            self.console_view.brush.MoveCursor(row=offset_rows + h)
+            line = offset_str
+            line += self.console_view.brush.BgColor(color=8 if h % 2 else 15, bits=ape.Brush.ColorBits.Bit8) + ' ' * width
             self.console_view.brush.print(line + self.console_view.brush.ResetColor(), end='')
 
 
@@ -43,10 +72,16 @@ def main():
                                    borderless=False)
     pane.title = 'ASCII Painter'
 
-    color_pane = Colors8BitPalette(console_view=console_view, x=0, y=0,
-                                   alignment=ape.Alignment.RightBottom,
-                                   dimensions=ape.DimensionsFlag.Absolute)
-    pane.add_widget(color_pane)
+    widget = Colors8BitPalette(console_view=console_view, x=0, y=0,
+                               alignment=ape.Alignment.RightBottom,
+                               dimensions=ape.DimensionsFlag.Absolute)
+    pane.add_widget(widget)
+
+    widget = BrushWidget(console_view=console_view, x=17, y=0,
+                         alignment=ape.Alignment.RightBottom,
+                         dimensions=ape.DimensionsFlag.Absolute)
+
+    pane.add_widget(widget)
 
     console_view.add_widget(pane)
 
